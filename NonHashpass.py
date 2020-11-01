0b1bf754f162ec2a1c3d5681e9e1bc0432b867e8
NotStatilko <valerastatilko@gmail.com>
2020-11-01 22:56:40 +0200

    Initial commit

---
+++ NonHashpass.py
@@ -0,0 +1,160 @@
+from PyQt5 import QtWidgets, QtCore
+from nonhashpass import Ui_mainWindow
+from multiprocessing import Queue, Process
+from moduleNonHashpass import hashpass
+from sys import exit as sys_exit
+from time import time, sleep
+
+class nonhashpass(QtWidgets.QMainWindow):
+    def __init__(self):
+        start_time = time()
+        hashpass('','',10_000)
+        self.pbar_time = time() - start_time #For progressbar
+
+        super(nonhashpass, self).__init__()
+        self.ui = Ui_mainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.label_6.hide() #int error label
+        
+        self.ledits = {
+            self.ui.label_11: self.ui.lineEdit,
+            self.ui.label_10: self.ui.lineEdit_2,
+            self.ui.label_9:  self.ui.lineEdit_3,
+        }
+        for i in self.ledits:
+            i.hide()
+        
+        self.ui.pushButton_2.hide() #generate another
+        self.ui.pushButton_3.hide() #stop
+        self.ui.label_7.hide() #in process label
+        self.ui.progressBar.resize(311,25)
+        self.ui.progressBar.hide() #progress bar
+
+        self.ui.label_8.hide() #resulted password
+        self.ui.lineEdit_4.hide() #resulted readonly password
+        
+        #make password button is pushButton
+        #generate password is label_5
+       
+        self.ui.pushButton.clicked.connect(self.make_password_clicked)
+        self.ui.pushButton_2.clicked.connect(self.erase_to_start)
+        self.ui.pushButton_3.clicked.connect(self.erase_to_start)
+
+        self.ui.pushButton.setDefault(True)
+        self.ui.pushButton_2.setDefault(True)
+        self.ui.pushButton_3.setDefault(True)
+
+        for i in self.ledits.values():
+            i.textChanged.connect(self.hide_with_edit)
+        
+        self.ui.lineEdit_3.textChanged.connect(self.check_int)
+        
+        self.generate_stopped = False
+        self.all_is_valid = False
+        self.active_daemon = None
+
+    def hide_all_empty_errors(self):
+        for i in self.ledits: i.hide()
+    
+    def make_password_clicked(self):
+        self.hide_all_empty_errors()
+        check_var = True
+        for k,v in self.ledits.items():
+            if not v.text():
+                k.show(); check_var = False
+
+        if not check_var:
+            self.all_is_valid = False
+        else:
+            self.all_is_valid = True
+
+        if self.all_is_valid:
+            self.ui.pushButton.hide()
+            self.ui.label_5.hide()
+            self.ui.label_7.show()
+            self.ui.pushButton_3.show()
+            self.ui.progressBar.show()
+            
+            percent = int(self.ui.lineEdit_3.text()) / 10_000 * self.pbar_time / 100
+
+            queue = Queue()
+            p = Process(target=hashpass,args=(
+                self.ui.lineEdit.text(),
+                self.ui.lineEdit_2.text(),
+                int(self.ui.lineEdit_3.text()), queue
+            ))
+            p.daemon = True; p.start()
+            self.active_daemon = p
+
+            for i in range(100):
+                i += 1
+                if self.generate_stopped:
+                    break
+                if not queue.qsize():
+                    updated_time = time()
+                    while time() - updated_time < percent:
+                        if self.generate_stopped:
+                            break
+                        QtCore.QCoreApplication.processEvents()
+                    else:
+                        self.ui.progressBar.setValue(i)
+                else:
+                    self.ui.progressBar.setValue(100)
+                    break
+            
+            if not self.generate_stopped:
+                p.join()
+                self.ui.progressBar.setValue(0)
+                self.ui.label_7.hide()
+                self.ui.label_8.show()
+                self.ui.lineEdit_4.setText(queue.get())
+                self.ui.lineEdit_4.show()
+                self.ui.pushButton_2.show()
+                self.ui.pushButton_3.hide()
+                self.active_daemon = None
+            
+            self.generate_stopped = False
+
+    def hide_with_edit(self):
+        self.hide_all_empty_errors()
+    
+    def check_int(self):
+        if self.ui.lineEdit_3.text():
+            if not self.ui.lineEdit_3.text().isnumeric():
+                self.ui.label_6.show(); self.all_is_valid = False
+            else:
+                self.ui.label_6.hide(); self.all_is_valid = True
+        else:
+            self.ui.label_6.hide(); self.all_is_valid = False
+    
+    def erase_to_start(self):
+        if self.active_daemon:
+            self.generate_stopped = True
+            self.active_daemon.terminate()
+            self.active_daemon.join()
+            self.active_daemon = None
+
+        self.ui.progressBar.setValue(0)
+        self.ui.pushButton_3.hide()
+        self.ui.label_7.hide()
+        self.ui.progressBar.hide()
+        self.ui.pushButton_2.hide()
+        self.ui.label_8.hide()
+        self.ui.lineEdit_4.hide()
+        self.ui.label_5.show()
+        self.ui.pushButton.show()
+
+    def closeEvent(self,event):
+        if self.active_daemon:
+            self.active_daemon.terminate()
+            self.active_daemon.join()
+        
+        self.close(); sys_exit()
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    application = nonhashpass()
+    application.show()
+
+    sys_exit(app.exec())
---
+++ moduleNonHashpass.py
@@ -0,0 +1,53 @@
+import random
+
+from hashlib import (
+    sha3_256, blake2b, sha3_512, shake_256, 
+    sha1, shake_128, sha384, sha512, sha224, 
+    blake2s, sha3_224, sha256, md5, sha3_384
+)
+from itertools import cycle
+
+hashfuncs = [
+    sha3_256, blake2b, sha3_512, shake_256, 
+    sha1, shake_128, sha384, sha512, sha224, 
+    blake2s, sha3_224, sha256, md5, sha3_384
+]
+def hashpass(phrase: str, unique_word: str, iterations: int, queue=None) -> shake_256:
+    '''
+    Args:
+        phrase (str): Bitcoin's BIP39 phrase or your password. Will be used as a 
+           master-key for all your passwords.
+
+        unique_word (str): Unique word to restore your password. It can be a name
+            of service or anything like this. E.g "gmail: mail@gmail.com".
+
+        iterations (int): Number of iterations over hash functions. You can set this as
+            a constant number or change it for every service. Anyway i STRONGLY
+            suggest you choose a big number (starting from 1,000,000) to
+            fully protect you from bruteforcing. The larger this value, the longer 
+            it will take to generate the password and the stronger the protection.  
+            Don't choose too obvious for attacker numbers.
+
+    Returns:
+        The shake_256 object. You can get hash of x bytes with "returned.digest(x)"
+
+    Raises:
+        TypeError: If you pass to function bytes instead of str.
+    '''
+    initkey = sha3_512(''.join((phrase, unique_word, str(iterations))).encode()).digest()
+    # initkey is seed of PRNG which shuffle list of hashfuncs
+
+    random_seed = random; random_seed.seed(initkey)
+    hashfuncs_copy = hashfuncs[:]; random_seed.shuffle(hashfuncs_copy)
+    cycled_hashfuncs_copy = cycle(hashfuncs_copy) # cycled shuffled hashfuncs
+
+    for _ in range(iterations):
+        hashfunc = next(cycled_hashfuncs_copy)
+        initkey = hashfunc(initkey)
+        try:
+            initkey = initkey.digest()
+        except TypeError: #shake requires digest size
+            initkey = initkey.digest(64)
+   
+    if queue:
+        queue.put(shake_256(initkey).hexdigest(32))
